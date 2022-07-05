0126ea3c99a0ee1702e73c0528731caf6e60caac
dennis <denizponce16@gmail.com>
2022-07-05 11:37:32 -0500

    cambios en navbar.module.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styles from "../styles/navbar.module.css";
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -11,8 +12,12 @@ const NavBar = () => {
 
   return (
     <div>
-      <Link to="/shop">Shop</Link>
-      <Link to="/cart">Cart</Link>
+      <Link className={styles.btnMenu} to="/shop">
+        Shop
+      </Link>
+      <Link className={styles.btnMenu} to="/cart">
+        Cart
+      </Link>
 
       <button onClick={logOut}>Log out</button>
     </div>
